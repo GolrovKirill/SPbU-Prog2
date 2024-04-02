3b46903cafc4ab7aa51a90be73abe7905ece1cff
Kirill Gorlov <st117367@student.spbu.ru>
2024-04-02 17:21:49 +0300

    Added Tests

--- Trie/Program.cs
+++ Trie/Program.cs
@@ -1,10 +1,10 @@
-﻿using static Trie.TrieVertices;
+﻿namespace Trie;
 
-namespace Trie;
+using static Trie.TrieVertices;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    private static void Main()
     {
         var trie = new TrieVertices();
         Console.WriteLine("Start Trie");
@@ -14,13 +14,13 @@ class Program
         {
             Console.Write("WriteLine: Add - 1, Contains - 2, Remove - 3, StartsWithPrefix - 4, Size - 5, Exit - 6   ");
             var cnt = Console.ReadLine();
-            
+
             switch (cnt)
             {
                 case "1":
                     Console.Write("Enter a word: ");
                     var str = Console.ReadLine();
-                    
+
                     if (string.IsNullOrEmpty(str))
                     {
                         Console.WriteLine("Input string empty");
@@ -35,13 +35,13 @@ class Program
                     {
                         Console.WriteLine("Successfully Add");
                     }
-                    
+
                     break;
-                
+
                 case "2":
                     Console.Write("Enter a word: ");
                     str = Console.ReadLine();
-                    
+
                     if (string.IsNullOrEmpty(str))
                     {
                         Console.WriteLine("Input string empty");
@@ -56,13 +56,13 @@ class Program
                     {
                         Console.WriteLine("Found");
                     }
-                    
+
                     break;
-                
+
                 case "3":
                     Console.Write("Enter a word: ");
                     str = Console.ReadLine();
-                    
+
                     if (string.IsNullOrEmpty(str))
                     {
                         Console.WriteLine("Input string empty");
@@ -75,32 +75,32 @@ class Program
                     }
                     else
                     {
-                        Console.WriteLine("Successfully Remove"); 
+                        Console.WriteLine("Successfully Remove");
                     }
-                    
+
                     break;
-                
+
                 case "4":
                     Console.Write("Enter a word: ");
                     str = Console.ReadLine();
-                    
+
                     if (string.IsNullOrEmpty(str))
                     {
                         Console.WriteLine("Input string empty");
                         break;
                     }
-                    
+
                     Console.WriteLine(trie.HowManyStartsWithPrefix(str));
                     break;
-                
+
                 case "5":
                     Console.WriteLine(trie.Size());
                     break;
-                
+
                 case "6":
                     end = false;
                     break;
-                
+
                 default:
                     Console.WriteLine("Input Error");
                     break;
--- Trie/TrieVertices.cs
+++ Trie/TrieVertices.cs
@@ -7,36 +7,37 @@ public class TrieVertices
         /// <summary>
         /// Stores the number of words starting with a prefix ending with this vertex
         /// </summary>
-        public int Prefix { get; set; } 
-        
+        public int Prefix { get; set; }
+
         /// <summary>
         /// Creates a dictionary of subvertices
         /// </summary>
         public Dictionary<char, Vertex> SubVertices { get; set; }
 
         /// <summary>
+        /// Initializes a new instance of the <see cref="Vertex"/> class.
         /// Fills in the fields of the subvertices
         /// </summary>
         public Vertex()
         {
-            Flag = false;
-            Prefix = 0;
-            SubVertices = new Dictionary<char, Vertex>();
-        } 
-        
+            this.Flag = false;
+            this.Prefix = 0;
+            this.SubVertices = new Dictionary<char, Vertex>();
+        }
+
         /// <summary>
-        /// Shows whether the top is the end of someone's word
+        /// Gets or sets a value indicating whether shows whether the top is the end of someone's word.
         /// </summary>
-        public bool Flag { get; set; } 
-        
+        public bool Flag { get; set; }
+
         /// <summary>
         /// Used to find a sub-vertex
         /// </summary>
-        /// <param name="symbol"></param>
         /// <returns>If there is such a subvertex with this symbol true, otherwise false</returns>
-        public Vertex FindSubVertex(char symbol)
+        public Vertex? FindSubVertex(char symbol)
         {
-            if (SubVertices.TryGetValue(symbol, out var value))
+            ArgumentOutOfRangeException.ThrowIfNegative(symbol);
+            if (this.SubVertices.TryGetValue(symbol, out var value))
             {
                 return value;
             }
@@ -46,61 +47,70 @@ public class TrieVertices
             }
         }
     }
+
     /// <summary>
     /// Initializing the root
     /// </summary>
-    protected Vertex root = new Vertex(); 
-    
+    protected Vertex? root = new Vertex();
+
     /// <summary>
     /// Adding a word to Trie
     /// </summary>
-    /// <param name="element"></param>
-    /// <returns>If you manage to add a word to Trie then true, otherwise false</returns>
-    public bool Add(string element) 
+    /// <returns>If you manage to add a word to Trie then true, otherwise false.</returns>
+    public bool Add(string element)
     {
-        if (Contains(element))
+        if (element == null)
+        {
+            throw new ArgumentNullException(nameof(element));
+        }
+
+        if (this.Contains(element))
         {
             return false;
         }
-        
-        Vertex current = root;
-        
+
+        Vertex? current = this.root;
+
         foreach (var symbol in element)
         {
-            var subvertex = current.FindSubVertex(symbol);
+            var subvertex = current?.FindSubVertex(symbol);
             current.Prefix += 1;
-            
+
             if (subvertex == null)
             {
                 subvertex = new Vertex();
                 current.SubVertices.Add(symbol, subvertex);
             }
-            
+
             current = subvertex;
         }
-        
+
         current.Flag = true;
         return current.Flag;
     }
-    
+
     /// <summary>
     /// Checks if Trie contains a word
     /// </summary>
-    /// <param name="element"></param>
     /// <returns>If it contains a word in Trie then true, otherwise false</returns>
     public bool Contains(string element)
     {
-        Vertex current = root;
-        
+        if (element == null)
+        {
+            throw new ArgumentNullException(nameof(element));
+        }
+
+        Vertex? current = this.root;
+
         foreach (var symbol in element)
         {
-            var subvertex = current.FindSubVertex(symbol);
-            
+            var subvertex = current?.FindSubVertex(symbol);
+
             if (subvertex == null)
             {
                 return false;
             }
-            
+
             current = subvertex;
         }
 
@@ -110,20 +120,24 @@ public class TrieVertices
     /// <summary>
     /// Removes a word from a Trie
     /// </summary>
-    /// <param name="element"></param>
-    /// <returns>If you succeed in removing a word from Trie then true, otherwise false</returns>
+    /// <returns>If you succeed in removing a word from Trie then true, otherwise false.</returns>
     public bool Remove(string element)
     {
-        if (!Contains(element))
+        if (element == null)
+        {
+            throw new ArgumentNullException(nameof(element));
+        }
+
+        if (!this.Contains(element))
         {
             return false;
         }
-        
-        Vertex current = root;
-        
+
+        Vertex? current = this.root;
+
         foreach (var symbol in element)
         {
-            var subvertex = current.FindSubVertex(symbol);
+            var subvertex = current?.FindSubVertex(symbol);
             current.Prefix -= 1;
             current = subvertex;
         }
@@ -131,25 +145,29 @@ public class TrieVertices
         current.Flag = false;
         return !current.Flag;
     }
-    
+
     /// <summary>
     /// Counts the number of words starting with a given prefix in Trie
     /// </summary>
-    /// <param name="prefix"></param>
-    /// <returns>Word count</returns>
+    /// <returns>Word count.</returns>
     public int HowManyStartsWithPrefix(string prefix)
     {
-        Vertex current = root;
-        
+        if (prefix == null)
+        {
+            throw new ArgumentNullException(nameof(prefix));
+        }
+
+        Vertex? current = this.root;
+
         foreach (var symbol in prefix)
         {
-            var subvertex = current.FindSubVertex(symbol);
-            
+            var subvertex = current?.FindSubVertex(symbol);
+
             if (subvertex == null)
             {
                 return 0;
             }
-            
+
             current = subvertex;
         }
 
@@ -159,9 +177,9 @@ public class TrieVertices
     /// <summary>
     /// Counts how many words there are in a Trie
     /// </summary>
-    /// <returns>Word count</returns>
+    /// <returns>Word count.</returns>
     public int Size()
     {
-        return root.Prefix;
+        return this.root.Prefix;
     }
 }
